e0df79fae66897bcb946330071cc2cfc19bb8453
s t e i n <stein@stein.rip>
2023-07-06 18:06:58 -0400

    new api key

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { Movie } from "../models/Movie";
 import {
-  getMovies,
-  getMoviesBySearch,
-  getTrendingMovies,
+	getMovies,
+	getMoviesBySearch,
+	getTrendingMovies,
 } from "../services/TMDBService";
 
 import SearchForm from "./SearchForm";
@@ -13,32 +13,32 @@ import { useSearchParams } from "react-router-dom";
 import CardList from "./Cardlist";
 
 const Home = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [searchParams] = useSearchParams();
-  const search = searchParams.get("search");
-  const genre = searchParams.get("genre");
-  const rating = searchParams.get("rating");
-  const length = searchParams.get("length");
-  useEffect(() => {
-    (async () => {
-      if (search) {
-        const movies: Movie[] = (await getMoviesBySearch(search)).results;
-        setMovies(movies);
-      } else if (genre || rating || length) {
-        const movies: Movie[] = (await getMovies(genre, rating, length))
-          .results;
-        setMovies(movies);
-      } else {
-        const movies: Movie[] = (await getTrendingMovies()).results;
-        setMovies(movies);
-      }
-    })();
-  }, [genre, rating, length, search]);
-  return (
-    <div className="Home">
-      <SearchForm />
-      <CardList allmovies={movies} />
-    </div>
-  );
+	const [movies, setMovies] = useState<Movie[]>([]);
+	const [searchParams] = useSearchParams();
+	const search = searchParams.get("search");
+	const genre = searchParams.get("genre");
+	const rating = searchParams.get("rating");
+	const length = searchParams.get("length");
+	useEffect(() => {
+		(async () => {
+			if (search) {
+				const movies: Movie[] = (await getMoviesBySearch(search)).results;
+				setMovies(movies);
+			} else if (genre || rating || length) {
+				const movies: Movie[] = (await getMovies(genre, rating, length))
+					.results;
+				setMovies(movies);
+			} else {
+				const movies: Movie[] = (await getTrendingMovies()).results;
+				setMovies(movies);
+			}
+		})();
+	}, [genre, rating, length, search]);
+	return (
+		<div className="Home">
+			<SearchForm />
+			<CardList allmovies={movies} />
+		</div>
+	);
 };
 export default Home;
